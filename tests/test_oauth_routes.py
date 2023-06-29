4dbbfe4d522a83b45ed50d6ad8c54a452c11bacb
errbufferoverfl <merrymet@errbufferoverfl.me>
2023-06-29 20:44:50 +1000

    Removes support for the Implicit grant (response_type=token)

--- tests/test_oauth_routes.py
+++ tests/test_oauth_routes.py
@@ -5,18 +5,18 @@ def test_authorize(client):
     assert client.get('/authorize').status_code == 200
 
 
-@pytest.mark.parametrize(('client_id', 'response_type', 'redirect_uri', 'message'), (
-        ("", "code", "https://example.com", "mandatory_parameter_missing"),
-        ("", "token", "https://example.com", "mandatory_parameter_missing"),
-        # ("A123456", "code", "https://example.com", ""),
-        # ("A123456", "token", "https://example.com", ""),
-        ("A123456", "code", "", "mandatory_parameter_missing"),
-        ("A123456", "token", "", "mandatory_parameter_missing"),
+@pytest.mark.parametrize(('client_id', 'redirect_uri', 'message'), (
+        ("", "https://example.com", "mandatory_parameter_missing"),
+        ("", "https://example.com", "mandatory_parameter_missing"),
+        # ("A123456", "https://example.com", ""),
+        # ("A123456", "https://example.com", ""),
+        ("A123456", "", "mandatory_parameter_missing"),
+        ("A123456", "", "mandatory_parameter_missing"),
 ))
-def test_request_example(client, client_id, response_type, redirect_uri, message):
+def test_request_example(client, client_id, redirect_uri, message):
     response = client.get(
         "/api/authorize",
-        query_string={"client_id": client_id, "response_type": response_type, "redirect_uri": redirect_uri}
+        query_string={"client_id": client_id, "redirect_uri": redirect_uri}
     )
 
     error_msg = response.json
